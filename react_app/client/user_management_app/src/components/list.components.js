119b680c8d88171a26639eec8d7bfcd1d7e6f9c6
ohmprakash1506 <ohmprakash96@outlook.com>
2023-12-18 17:38:24 +0530

    list component completed

--- react_app/client/user_management_app/src/components/list.components.js
+++ react_app/client/user_management_app/src/components/list.components.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import UserDataService from "../service/user.service";
 import { Link } from "react-router-dom";
+import UserDataService from "../service/user.service";
 
 export default class UsersList extends Component {
   constructor(props) {
     super(props);
-    this.onChangeSearchUser = this.onChangeSearchUser.bind(this);
-    this.retrieveUsers = this.retrieveUsers.bind(this);
-    this.refreshList = this.refreshList.bind(this);
-    this.removeAllUsers = this.removeAllUsers.bind(this);
-    this.searchUser = this.searchUser.bind(this);
-
     this.state = {
-      users: [],
-      currentUser: null,
-      currentIndex: -1,
       searchUser: "",
+      users: [],
+      filteredUsers: [],
     };
   }
 
@@ -23,58 +16,12 @@ export default class UsersList extends Component {
     this.retrieveUsers();
   }
 
-  onChangeSearchUser(e) {
-    const searchUser = e.target.value;
-
-    this.setState({
-      searchUser: searchUser,
-    });
-  }
-
   retrieveUsers() {
     UserDataService.getAll()
       .then((response) => {
         this.setState({
-          tutorials: response.data,
-        });
-        console.log(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  refreshList() {
-    this.retrieveUsers();
-    this.setState({
-      currentUser: null,
-      currentIndex: -1,
-    });
-  }
-
-  setActiveTutorial(users, index) {
-    this.setState({
-      currentUser: users,
-      currentIndex: index
-    });
-  }
-
-  removeAllUsers() {
-    UserDataService.deleteAll()
-      .then((response) => {
-        console.log(response.data);
-        this.refreshList();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  searchUser() {
-    UserDataService.findByUser(this.state.searchUser)
-      .then((response) => {
-        this.setState({
-          users: response.data,
+          users: response.data.response.data,
+          filteredUsers: response.data.response.data,
         });
         console.log(response.data);
       })
@@ -82,9 +29,17 @@ export default class UsersList extends Component {
         console.log(error);
       });
   }
+
+  handleSearch = () => {
+    const { searchUser, users } = this.state;
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(searchUser.toLowerCase())
+    );
+    this.setState({ filteredUsers });
+  };
 
   render() {
-    const { searchUser, users, currentIndex, currentUser } = this.state;
+    const { searchUser, filteredUsers } = this.state;
 
     return (
       <div className="list row">
@@ -93,82 +48,49 @@ export default class UsersList extends Component {
             <input
               type="text"
               className="form-control"
-              placeholder="Search by name or id"
+              placeholder="Search by name"
               value={searchUser}
-              onChange={this.onChangeSearchUser}
+              onChange={(e) => this.setState({ searchUser: e.target.value })}
             />
             <div className="input-group-append">
               <button
                 className="btn btn-outline-secondary"
                 type="button"
-                onClick={this.searchTitle}
+                onClick={this.handleSearch}
               >
                 Search
               </button>
             </div>
           </div>
         </div>
-        <div className="col-md-6">
-            <h3>Users List</h3>
-
-            <ul className="list-group">
-            {users &&
-              users.map((users, index) => (
-                <li
-                  className={
-                    "list-group-item " +
-                    (index === currentIndex ? "active" : "")
-                  }
-                  onClick={() => this.setActiveUser(users, index)}
-                  key={index}
-                >
-                  {users.firstName}
-                </li>
+        <div className="col-md-12">
+          <h3>Users List</h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Contact</th>
+                <th>Date of Birth</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.map((user) => (
+                <tr key={user._id}>
+                  <td>
+                    <Link to={`/user-details/${user._id}`}>{user.name}</Link>
+                  </td>
+                  <td>{user.emailID}</td>
+                  <td>{user.contact}</td>
+                  <td>{user.dateOfbirth}</td>
+                  <td>{user.age}</td>
+                </tr>
               ))}
-          </ul>
-
-          <button
-            className="m-3 btn btn-sm btn-danger"
-            onClick={this.removeAllUsers}
-          >
-            Remove All
-          </button>
+            </tbody>
+          </table>
         </div>
-        <div className="col-md-6">
-        {currentUser ? (
-            <div>
-              <h4>Users</h4>
-              <div>
-                <label>
-                  <strong>Name:</strong>
-                </label>{" "}
-                {currentUser.firstName}
-              </div>
-              <div>
-                <label>
-                  <strong>Description:</strong>
-                </label>{" "}
-                {currentUser.description}
-              </div>
-              <div>
-                <label>
-                  <strong>Status:</strong>
-                </label>{" "}
-                {currentUser.published ? "Published" : "Pending"}
-              </div>
-
-              <Link
-                to={"/tutorials/" + currentUser.id}
-                className="badge badge-warning"
-              >
-                Edit
-              </Link>
-        </div>
-    ):(
-        <div></div>
-    )}
-    </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
